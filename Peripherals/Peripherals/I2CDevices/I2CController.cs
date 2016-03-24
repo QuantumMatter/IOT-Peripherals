353316a235a49a621389d92fbf15fe56d3cfbe8b
QuantumMatter <davidk@ihcconline.com>
2016-03-24 13:43:29 -0600

    Created projects; Started: AdafruitPWM.cs, MPU6050.cs, I2CController.cs

---
+++ Peripherals/Peripherals/I2CDevices/I2CController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace Peripherals.I2CDevices
+{
+    class I2CController
+    {
+        I2cDevice device = null;
+        public byte addr;
+
+        public I2CController(byte address)
+        {
+            addr = address;
+            InitializeComponent();
+        }
+
+        public async void InitializeComponent()
+        {
+            string aqs = I2cDevice.GetDeviceSelector();
+            DeviceInformationCollection collection = await DeviceInformation.FindAllAsync(aqs);
+
+            I2cConnectionSettings settings = new I2cConnectionSettings(addr);
+            settings.BusSpeed = I2cBusSpeed.FastMode; // 400kHz clock
+            settings.SharingMode = I2cSharingMode.Exclusive;
+            device = await I2cDevice.FromIdAsync(collection[0].Id, settings);
+        }
+
+        public byte ReadByte(byte regAddr)
+        {
+            byte[] buffer = new byte[1];
+            buffer[0] = regAddr;
+            byte[] value = new byte[1];
+            device.WriteRead(buffer, value);
+            return value[0];
+        }
+
+        public byte[] ReadBytes(byte regAddr, int length)
+        {
+            byte[] values = new byte[length];
+            byte[] buffer = new byte[1];
+            buffer[0] = regAddr;
+            device.WriteRead(buffer, values);
+            return values;
+        }
+
+        public void WriteByte(byte regAddr, byte data)
+        {
+            byte[] buffer = new byte[2];
+            buffer[0] = regAddr;
+            buffer[1] = data;
+            device.Write(buffer);
+        }
+
+        public void writeBytes(byte regAddr, byte[] values)
+        {
+            byte[] buffer = new byte[1 + values.Length];
+            buffer[0] = regAddr;
+            Array.Copy(values, 0, buffer, 1, values.Length);
+            device.Write(buffer);
+        }
+    }
+}
---
+++ Peripherals/Peripherals/I2CDevices/Output/AdafruitPWM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+
+namespace Peripherals.I2CDevices.Output
+{
+    class AdafruitPWM
+    {
+
+        #region byte constants
+        //Registers
+        byte MODE1 =            0x00;
+        byte MODE2 =            0x01;
+        byte SUBADR1 =          0x02;
+        byte SUBADR2 =          0x03;
+        byte SUBADR3 =          0x04;
+        byte PRESCALE =         0xFE;
+        byte LED0_ON_L =        0x06;
+        byte LED0_ON_H =        0x07;
+        byte LED0_OFF_L =       0x08;
+        byte LED0_OFF_H =       0x09;
+        byte ALL_LED_ON_L =     0xFA;
+        byte ALL_LED_ON_H =     0xFB;
+        byte ALL_LED_OFF_L =    0xFC;
+        byte ALL_LED_OFF_H =    0xFD;
+
+        //Bits
+        byte RESTART =          0x80;
+        byte SLEEP =            0x10;
+        byte ALLCALL =          0x01;
+        byte INVRT =            0x10;
+        byte OUTDRV =           0x04;
+
+        #endregion
+
+        public byte addr = 0x40;
+
+        public AdafruitPWM(byte address)
+        {
+            addr = address;
+            InitializeHardware();
+        }
+
+        public async void InitializeHardware()
+        {
+            
+        }
+    }
+}
